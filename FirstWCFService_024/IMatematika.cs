728e68a023fa688b085f9426542ba46c76a92bc1
Yoo <79823445+asthey@users.noreply.github.com>
2021-10-10 17:59:38 +0700

    Add project files.

---
+++ FirstWCFService_024/IMatematika.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FirstWCFService_024
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        /* output->*/
+        int Tambah(int a, int b); //method
+        [OperationContract]
+        int Kurang(int a, int b); //input
+        [OperationContract]
+        int Kali(int a, int b);
+        [OperationContract]
+
+        int Bagi(int a, int b);
+        [OperationContract]
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
+    }
+    [DataContract]
+    public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember] //membuka koordinat, x,y
+        public int X //properties
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+
+        }
+    }
+}
---
+++ FirstWCFService_024/Matematika.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FirstWCFService_024
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Matematika : IMatematika
+    {
+        public int Bagi(int a, int b)
+        {
+            return a / b;
+        }
+        public int Kali(int a, int b)
+        {
+            return a * b;
+        }
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            Koordinat hasil = new Koordinat();
+            hasil.X = a.X + b.X;
+            hasil.Y = a.Y + b.Y;
+            return hasil;
+        }
+    }
+}
